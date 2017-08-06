4b996c507ff34c5db0faed0e4597483bfdece0d6
hedgehog125 <username@users.noreply.github.com>
2017-08-06 20:23:58 +0100

    It should now actually work. :P

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import AutoUpdate
+import AutoUpdate, os
 AutoUpdate.database = "https://raw.githubusercontent.com/hedgehog125/Itch-Server_Database/master/"
 AutoUpdate.init()
 exec(open("Assets/Server.py").read())
